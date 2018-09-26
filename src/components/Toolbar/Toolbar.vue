24d08b6db153379f7a0316ce2ce18ecb765c8363
Moikapy <warrenckhan@gmail.com>
2018-09-25 22:52:05 -0400

    creating items/option for toolbar

--- src/components/Toolbar/Toolbar.vue
+++ src/components/Toolbar/Toolbar.vue
@@ -1,21 +1,18 @@
 <template>
-    <nav class="toolbar navbar justify-content-start px-0">
-        <li class="nav-item no-list-style dropdown m-0 px-2">
-            <a class="dropdown-toggle nav-link p-0 m-0" dropdown-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" id="navbarDropdown">File</a>
-            <div class="dropdown-menu"  aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <a class="dropdown-item" href="#">Something else here</a>
-                <div class="dropdown-divider" />
-                <a class="dropdown-item" href="#">Separated link</a>
-            </div>
+    <nav class="toolbar px-0">
+        <div class="navbar justify-content-start">
+        <li class="nav-item nav-link pl-0 file-info">
+            File
         </li>
-        <li class="nav-link nav-item px-2"><router-link to="/vr">VR</router-link></li>
-        <li class="nav-link nav-item px-2"><router-link to="/settings">Settings</router-link></li>
+        <li class="nav-link nav-item ml-1"><router-link to="/">Edit</router-link></li>
+        <li class="nav-link nav-item "><router-link to="/vr">VR</router-link></li>
+        <li class="nav-link nav-item "><router-link to="/settings">Settings</router-link></li>
+        <li>Help</li>
+        </div>
     </nav> 
 </template>
 <style>
-.toolbar {
+.toolbar .navbar{
   height: 10px;
   background-color: #eee;
 }
@@ -25,9 +22,18 @@
   text-decoration: none;
 }
 .nav-item {
-  font-size: 12px;
+  font-size: 10.50px;
+  font-weight: 500
 }
+.file-info{
+    position: absolute;
+    left: 7px;
+}
+
 .no-list-style{
     list-style: none !important;
+}
+.dropdown-toggle::after{
+    display: none !important;
 }
 </style>

281f168a2386f51fafdc624bc9c7dc63e64ce731
moikapy <warrenckhan@gmail.com>
2018-10-03 01:55:10 -0400

    - created global css file for custom resuables. - added dropdowns to the toolbar - created dropdown mixin - added gloabal style sheet to main.js - tweaked readme

--- src/components/Toolbar/Toolbar.vue
+++ src/components/Toolbar/Toolbar.vue
@@ -1,68 +1,95 @@
 <template>
-    <nav class="toolbar px-0 navbar justify-content-start">
-        <li class="pl-1 toolbar-item no-list-style file-info">
-            File
-        </li>
-        <li class="toolbar-item no-list-style  edit-info">
-            <router-link to="/">
-            Edit
-            </router-link>
-        </li>
-        <li class="toolbar-item no-list-style vr-info">
-            <router-link to="/vr">
-            VR
-            </router-link>
-        </li>
-        <li class="toolbar-item no-list-style settings-info">
-            <router-link to="/settings">
-            Settings
-            </router-link>
-        </li>
-        <li class="toolbar-item no-list-style help-info">
-            Help
-        </li>
-    </nav> 
+    <div>
+        <nav class="toolbar px-0 navbar justify-content-start">
+            <li class="pl-1 toolbar-item no-list-style file-info">
+                <Dropdown>
+                    <template slot="btn">File</template>
+                    <template slot="body">
+                        <ul class="d-flex flex-row pl-0 pr-3 m-0">
+                            <li class="btn no-list-style fnt-11" :click="newFile">New</li>
+                            <li class="btn no-list-style fnt-11" :click="openFile">Open</li>
+                            <li class="btn no-list-style fnt-11" :click="saveFile">Save</li>
+                            <li class="btn toolbar-item no-list-style fnt-11">
+                                <router-link to="/settings">
+                                    Settings
+                                </router-link>
+                            </li>
+                        </ul>
+                    </template>
+                </Dropdown>
+            </li>
+            <li class="toolbar-item no-list-style  edit-info">
+                <Dropdown>
+                    <template slot="btn">Edit</template>
+                    <template slot="body">
+                        <ul class="d-flex flex-row pl-0 pr-3 m-0">
+                            <li class="btn no-list-style fnt-11" :click="saveFile">Undo</li>
+                        </ul>
+                    </template>
+                </Dropdown>
+            </li>
+            <li class="toolbar-item no-list-style vr-info">
+                <router-link to="/vr">
+                    VR
+                </router-link>
+            </li>
+            <li class="toolbar-item no-list-style help-info">
+                <Dropdown>
+                    <template slot="btn">Help</template>
+                    <template slot="body">
+                        <ul class="d-flex flex-row pl-0 pr-3 m-0">
+                            <li class="btn no-list-style fnt-11">Aframe Documentation</li>
+                            <li class="btn no-list-style fnt-11">VueJS Documentation</li>
+                            <li class="btn no-list-style fnt-11">Electron Documentation</li>
+                            <li class="btn toolbar-item no-list-style fnt-11">
+                                <router-link to="/settings">
+                                    Settings
+                                </router-link>
+                            </li>
+                        </ul>
+                    </template>
+                </Dropdown>
+            </li>
+        </nav>
+    </div>
 </template>
+<script>
+    import Dropdown from '@/mixins/dropdown';
+    const electron = require("electron"),
+        app = electron.app,
+        BrowserWindow = electron.BrowserWindow;
+    export default {
+        name: 'Toolbar',
+        components: {
+            Dropdown
+        },
+        methods: {
+            saveFile(e){
+                console.log('file save');
+            }
+        }
+    }
+</script>
 <style>
-.toolbar {
-  height: 10px;
-  background-color: #eee;
-}
-.toolbar a:hover,
-.toolbar a {
-  color: #000;
-  text-decoration: none;
-}
-.toolbar-item {
-  font-size: 10.5px;
-  font-weight: 500;
-  padding: .5rem .4rem;
-}
-.dropdown-toggle::after {
-  display: none !important;
-}
-/* .file-info{
-    position: absolute;
-    left: 7px;
-}
-.edit-info{
-    position: absolute;
-    left: 15px;
-}
-.vr-info{
-    position: absolute;
-    left: 53px;
-}
-.settings-info{
-    position: absolute;
-    left: 92px;
-}
-.help-info{
-    position: absolute;
-    left: 147px;
-} */
+    .toolbar {
+        height: 10px;
+        background-color: #eee;
+    }
 
-.no-list-style {
-  list-style: none !important;
-}
+    .toolbar a:hover,
+    .toolbar a {
+        color: #000;
+        text-decoration: none;
+    }
+
+    .toolbar-item {
+        font-size: 10.5px;
+        font-weight: 500;
+        padding: .5rem .4rem;
+    }
+
+    .dropdown-toggle::after {
+        display: none !important;
+    }
+
 </style>
